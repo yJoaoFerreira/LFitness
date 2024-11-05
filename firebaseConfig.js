20d9e5a3fe66a5aadf42b3e86e078d68856a2442
João Ferreira <yFerreiraJoao@gmail.com>
2024-11-05 14:38:34 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD7TaSnJJWlU2g1EHlw_U0NvKTosSo7xMc",
-  authDomain: "lfitness-e421c.firebaseapp.com",
-  projectId: "lfitness-e421c",
-  storageBucket: "lfitness-e421c.appspot.com",
-  messagingSenderId: "239375261689",
-  appId: "1:239375261689:web:cc882f3aa439a677aba728",
-  measurementId: "G-ZBQ56JE3EG"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
