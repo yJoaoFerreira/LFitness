3c8b6cbe85609e09b2219bb4c2112ed95ced6758
João Ferreira <yFerreiraJoao@gmail.com>
2024-11-11 17:52:23 -0300

    Update firebaseConfig.js

--- services/firebaseConfig.js
+++ services/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7TaSnJJWlU2g1EHlw_U0NvKTosSo7xMc",
-  authDomain: "lfitness-e421c.firebaseapp.com",
-  projectId: "lfitness-e421c",
-  storageBucket: "lfitness-e421c.appspot.com",
-  messagingSenderId: "239375261689",
-  appId: "1:239375261689:web:cc882f3aa439a677aba728",
-  measurementId: "G-ZBQ56JE3EG"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
